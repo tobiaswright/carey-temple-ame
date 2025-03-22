2311521d929c7d95a41b7fb1ccd9cdf71727c3cb
Tobias Wright <tobias.wright@gmail.com>
2025-03-22 10:01:09 -0500

    added to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://tobiaswright.github.io',
+  base: 'carey-temple-ame',
 });
